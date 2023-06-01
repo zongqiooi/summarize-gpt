a7191d6f1a962791704c75112f650a448bb7e80e
Zong Qi <huro710@hotmail.com>
2023-06-01 14:43:49 +0800

    Updated footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>Made with ❤️ by Zong Qi.</p>
+    </div>
+  );
+};
+
+export default Footer;
