e2f8bb82a71c1bb0f53de34801b708264d01ed18
Zong Qi <huro710@hotmail.com>
2023-06-01 14:44:01 +0800

    Updated hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full pt-3 mb-10"></nav>
+      <h1 className="head_text">
+        Articles summarizer with <br className="max-md:hidden" />
+        <span className="orange_gradient prevent-select">Summarize-GPT</span>
+      </h1>
+      <h2 className="desc">
+        AI-powered article summarizer to simplify your reading and writing.
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
