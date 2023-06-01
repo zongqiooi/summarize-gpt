243f7bd6459818ff8211f218c314bd17edb75074
Zong Qi <huro710@hotmail.com>
2023-06-01 14:46:21 +0800

    Updated main app and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Summary from "./components/Summary";
+import Footer from "./components/Footer";
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div className="app">
+        <Hero />
+        <Summary />
+        <Footer />
+      </div>
+    </main>
+  );
 };
 
 export default App;
